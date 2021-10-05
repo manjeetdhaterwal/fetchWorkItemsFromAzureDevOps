5402cf09b44e4df7d8edc9967fdbf52a3689227c
Manjeet Dhaterwal <67642088+manjeetdhaterwal@users.noreply.github.com>
2021-10-05 10:19:51 +0530

    Removed unwanted Code

--- demo app/demo app/Program.cs
+++ demo app/demo app/Program.cs
@@ -7,11 +7,6 @@ namespace demo_app
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            //QueryExecutor.Func();
-            //Class2.Try2();
-            //QueryExecutor2 queryExecutor2 = new QueryExecutor2();
-            //queryExecutor2.QueryOpenBugs();
-            //queryExecutor2.PrintOpenBugsAsync("eBH");
             Class4.try4();
             
         }
