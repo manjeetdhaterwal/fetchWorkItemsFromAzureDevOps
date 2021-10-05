6d0c4f11f3e60b088c2905a227afe3b6c532bc5d
Manjeet Dhaterwal <67642088+manjeetdhaterwal@users.noreply.github.com>
2021-10-05 10:18:37 +0530

    Added program.cs

---
+++ demo app/demo app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace demo_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //QueryExecutor.Func();
+            //Class2.Try2();
+            //QueryExecutor2 queryExecutor2 = new QueryExecutor2();
+            //queryExecutor2.QueryOpenBugs();
+            //queryExecutor2.PrintOpenBugsAsync("eBH");
+            Class4.try4();
+            
+        }
+    }
+}
